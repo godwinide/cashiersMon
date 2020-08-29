a445a803751b9981f4b17bdb5d9902e3cf490083
Godwin Idemudia <godwinide@gmail.com>
2020-08-29 11:40:50 +0100

    time changes

--- monitor.js
+++ monitor.js
@@ -4,9 +4,14 @@ function reset(){
     setInterval(() => {
         shell.exec("pm2 stop 1");
         setTimeout(()=> {
-            shell.exec("pm2 start 1");
-        },5000)
-    },3600000)
+            shell.exec("echo 1 > /proc/sys/vm/drop_caches");
+            shell.exec("echo 2 > /proc/sys/vm/drop_caches");
+            shell.exec("echo 3 > /proc/sys/vm/drop_caches");
+            setTimeout(()=> {
+                shell.exec("pm2 start 1");
+            },5000)
+        },15000)
+    },1800000)
 }
 
 reset();
