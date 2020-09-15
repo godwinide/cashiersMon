61b2fedae6302605dab4d8e5325ab9fd89fff9e5
Godwin Idemudia <godwinide@gmail.com>
2020-09-15 11:13:14 +0100

    ixed number issues

--- monitor.js
+++ monitor.js
@@ -2,16 +2,12 @@ const shell = require("shelljs");
 
 function reset(){
     setInterval(() => {
-        shell.exec("pm2 stop 1");
         setTimeout(()=> {
             shell.exec("echo 1 > /proc/sys/vm/drop_caches");
             shell.exec("echo 2 > /proc/sys/vm/drop_caches");
             shell.exec("echo 3 > /proc/sys/vm/drop_caches");
-            setTimeout(()=> {
-                shell.exec("pm2 start 1");
-            },5000)
         },5000)
-    },420000)
+    },220000)
 }
 
 reset();
