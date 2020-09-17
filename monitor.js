db11f0851d83d2369e4980bdaa2c54187b2df477
Godwin Idemudia <godwinide@gmail.com>
2020-09-17 10:11:09 +0100

    fixed errors

--- monitor.js
+++ monitor.js
@@ -2,12 +2,16 @@ const shell = require("shelljs");
 
 function reset(){
     setInterval(() => {
+        shell.exec("pm2 stop 1");
         setTimeout(()=> {
             shell.exec("echo 1 > /proc/sys/vm/drop_caches");
             shell.exec("echo 2 > /proc/sys/vm/drop_caches");
             shell.exec("echo 3 > /proc/sys/vm/drop_caches");
+            setTimeout(()=> {
+                shell.exec("pm2 start 1");
+            },5000)
         },5000)
-    },220000)
+    },300000)
 }
 
-reset();
+reset()
